818bb6cdbea9785cf75106fa48548630cdc4eca4
NiazMorshed2007 <niazmorshed2007@gmail.com>
2022-10-12 15:21:23 +0600

    update

--- index.py
+++ index.py
@@ -1,6 +1,17 @@
-s = "azcbobobegghakl"
-vowels = "aeiou"
+lists = []
+scores = []
+for _ in range(int(input("Enter number of students: "))):
+    name = input("Enter name: ")
+    score = float(input("Enter score: "))
+    lists.append([name, score])
+    scores.append(score)
 
-total_bobs = s.count("bob")
-total = [letter for letter in s if letter in vowels]
-print(total_bobs)
+min_score = min(scores)
+scores = filter(lambda val: val != min_score, scores)
+second_min_score = min(scores)
+answers = []
+for person in lists:
+    if person[1] == second_min_score:
+        answers.append(person[0])
+
+print(sorted(answers))
